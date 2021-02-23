394ae7b7078fca79119f4031f0fbcd681161c208
Victoria <vickycastrodotta@gmail.com>
2021-02-23 23:00:43 +0100

    Agregar archivos de proyecto.

---
+++ PeticionesPostPerros/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PeticionesPostPerros.Models;
+
+namespace PeticionesPostPerros.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Metodo GET:
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //////Ejemplo 1 sin modelos:
+        //[HttpPost]
+        //public IActionResult Index(String nombre, int edad, String raza, String descripcion)
+        //{
+        //    ViewData["nombre"] = nombre;
+        //    ViewData["edad"] = edad;
+        //    ViewData["raza"] = raza;
+        //    ViewData["descripcion"] = descripcion;
+        //    return View();
+        //}
+
+        //Ejemplo 2:
+        //[HttpPost]
+        //public IActionResult Index(String nombre, int edad, String raza, String descripcion)
+        //{
+        //    Perro perro = new Perro();
+        //    perro.Edad = edad;
+        //    perro.Nombre = nombre;
+        //    perro.Descripcion = descripcion;
+        //    perro.Raza = raza;
+        //    return View(perro);
+        //}
+
+        //Ejemplo 3 con binding model:
+        [HttpPost]
+        public IActionResult Index(Perro perro)
+        {
+
+            return View(perro);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PeticionesPostPerros/Models/Perro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeticionesPostPerros.Models
+{
+    public class Perro
+    {
+        public int Edad { get; set; }
+        public String Nombre { get; set; }
+        public String Raza { get; set; }
+        public String Descripcion { get; set; }
+    }
+}
